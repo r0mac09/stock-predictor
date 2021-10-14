fe22896724ff95d35b314b0733ab412cf9b3aac6
Alexandru Cristian Mihaescu <mihaescuac@gmail.com>
2021-10-14 23:48:41 +0300

    Added requirements.txt file

--- src/stocks.py
+++ src/stocks.py
@@ -7,9 +7,9 @@ class StockPuller:
     def __init__(self):
         pass
 
-    def pull(self, token, start, end):
+    def pull(self, token, start, end, interval='1d'):
         tickerData = yf.Ticker(token)
-        tickerDf = tickerData.history(interval='1d', start=start, end=end)
+        tickerDf = tickerData.history(interval=interval, start=start, end=end)
 
         if tickerDf.empty:
             raise Stock_NoData(token)
